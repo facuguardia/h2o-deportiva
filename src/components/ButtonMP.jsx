6aff821771790ab59ea5413f8d91be27a712b452
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2024-02-26 20:43:20 -0300

    Update button label to display price

--- src/components/ButtonMP.jsx
+++ src/components/ButtonMP.jsx
@@ -7,7 +7,7 @@ function ButtonMP({price}) {
       target="_blank"
       className="flex justify-center items-center px-10 py-2 mt-5 text-xl font-semibold text-white bg-[#009EE3] border-0 rounded w-[100%] focus:outline-none hover:bg-blue-600"
     >
-      <button>Pagar $ {price}</button>
+      <button><strong>$ {price}</strong></button>
     </Link>
   );
 }
